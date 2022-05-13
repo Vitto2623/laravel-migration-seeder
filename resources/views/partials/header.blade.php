84559c1cc7af3e99b40b1790faf385a6a79b6b17
Vitto2623 <vtr.1@icloud.com>
2022-05-13 14:42:19 +0200

    second commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">HomePage</a>
+    </div>
+</nav>
---
+++ resources/views/trains/index.blade.php
@@ -0,0 +1,23 @@
+@extends("layouts.main")
+
+@section('content')
+
+    <main>
+        <div class="container-fluid">
+            <div class="row text-center d-flex justify-content-center align-items-center p-5">
+                <div class="col-5 d-flex justify-content-center align-items-center m-0">
+                    <div class="card m-3" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title"><h4 class="text-danger">Titolo:</h4></h5>
+                            <p class="card-text"><h4 class="text-danger">Titolo originale:</h4></p>
+                            <p class="card-text"><h4 class="text-danger">Nazionalità:</h4></p>
+                            <p class="card-text"><h4 class="text-danger">Data di pubblicazione:</h4></p>
+                            <p class="card-text"><h4 class="text-danger">Voto:</h4></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    
+@endsection
